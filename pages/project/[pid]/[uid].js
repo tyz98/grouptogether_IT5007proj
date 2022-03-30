b73224190f826eb23107d8cda0a101403c6b598c
zhangtianyi <tianyizhang0424@gmail.com>
2022-03-31 02:58:49 +0800

    project-specific profile show page

--- pages/project/[pid]/[uid].js
+++ pages/project/[pid]/[uid].js
@@ -1,18 +1,38 @@
+import { useState, useEffect } from "react"
 import { useSession } from "next-auth/react"
+import { useRouter } from 'next/router'
 import AccessDenied from "../../../components/AccessDenied"
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import ReloadPrompt from "../../../components/ReloadPrompt"
+import { getProjectProfile } from "../../../actions/profile"
+import { Card, CardContent, Typography, Grid, FormGroup, FormControlLabel, Checkbox } from '@mui/material';
+
 
 export default function ProjectProfileShow(props) {
+  const router = useRouter()
+  const { pid, uid } = router.query
   const { data: session, status } = useSession();
   const loading = status === "loading";
+  const [questions, setQuestions] = useState([])
+  const [pageError, setPageError] = useState(false)
+
+  useEffect(() => {
+    if (!session) {
+      return;
+    }
+
+    getProjectProfile(pid, uid).then(res => { // res is exactly the data in response body
+      console.log("getProjectProfile res=", res)
+      setQuestions(res.projectProfile)
+      setPageError(false)
+    }).catch(err => { // err is {success: false, message: "error reason"}
+      if (err && err.message) {
+        alert(err.message)
+        setPageError(true)
+      } else {
+        setPageError(true)
+      }
+    })
+  }, [session, pid, uid])
 
   if (typeof window !== "undefined" && loading) return null
 
@@ -21,94 +41,50 @@ export default function ProjectProfileShow(props) {
         <AccessDenied />
     )
   }
-  const receivedInfo = {
-    basic: {
-      school:"National University of Singapore", 
-      email:"123@gmail.com",
-      name:"Ryan",
-      gender:"Male",
-      country:"China",
-      phone:"80391111",
-      image: 'https://source.unsplash.com/random',
-      imageLabel: 'Image Text',
-    }, 
-    specific: [
-      {desc:"frontend framewaork", options:["vue", "react"]}, 
-      {desc:"backend language", options:["java", "python"]},
-      {desc:"design experience", options:["familiar", "know", "never"]},
-    ]
-  };
 
-
-  const theme = createTheme();
-  const generateKey = (pre) => {
-    return `${ pre }_${ new Date().getTime() }`;
+  if (pageError) {
+    return (
+      <ReloadPrompt />
+    )
   }
-  
-  //TODO
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container maxWidth="lg">
-        <h1>User{props.uid}'s Project{props.pid}-Specific Profile Page</h1>
-        <Grid container spacing={4}>
-          <Grid item key={receivedInfo.basic.name} xs={6} md={6}>
-            <Card sx={{ display: 'flex' }}>
-              <CardMedia
-                  component="img"
-                  sx={{ width: 150, display: { xs: 'none', sm: 'block' } }}
-                  image={receivedInfo.basic.image}
-                  alt={receivedInfo.basic.imageLabel}
-              />
-              <CardContent sx={{ flex: 1 }}>
-                <Typography component="h2" variant="h5">
-                  {receivedInfo.basic.name}
-                </Typography>
-                <Typography variant="subtitle1" color="text.secondary">
-                  {receivedInfo.basic.gender}
-                </Typography>
-                <Typography variant="subtitle1" paragraph>
-                  {receivedInfo.basic.school}
-                </Typography>
-                <Typography variant="subtitle1" paragraph>
-                  {receivedInfo.basic.country}
-                </Typography>
-                <Typography variant="subtitle1" paragraph>
-                  {receivedInfo.basic.phone}
-                </Typography>
-                <Typography variant="subtitle1" color="primary">
-                  {receivedInfo.basic.email}
-                </Typography>
-              </CardContent>
+    <Grid container spacing={3}>
+      {
+        questions.map((question) => (
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={4} key={question._id}>
+            <Card sx={{ height: 300, overflow: "auto" }}>
+                <CardContent>
+                  <Typography variant="h6" component="div" gutterBottom>
+                    {question.desc}
+                  </Typography>
+                  <FormGroup>
+                  {question.chosenIdx.map((i)=> (
+                    <FormControlLabel 
+                      checked={true} 
+                      key={i} 
+                      name={i}
+                      label={question.options[i]}
+                      control={<Checkbox  />} 
+                    />))}
+                  {question.options.map((option, i)=> {
+                    if (question.chosenIdx.indexOf(i) !== -1) {
+                      return null
+                    }
+                    return(
+                    <FormControlLabel 
+                      checked={false} 
+                      disabled
+                      key={i} 
+                      name={i}
+                      label={option}
+                      control={<Checkbox  />} 
+                    />)})}
+                  </FormGroup>
+                </CardContent>
             </Card>
           </Grid>
-          {receivedInfo.specific.map((skill) => (
-            <Grid item key={generateKey(skill.desc)} xs={2} md={2}>
-              <Card sx={{ display: 'flex' }}>
-                <CardContent sx={{ flex: 1 }}>
-                  <Typography variant="h6">
-                    {skill.desc}
-                  </Typography>
-                  {skill.options.map((option) => (
-                    <Typography key={option} variant="subtitle1" paragraph>
-                      {option}
-                  </Typography>
-                  ))}
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-    </ThemeProvider>
-  )
-}
-
-export async function getServerSideProps(context) {
-  return {
-    props: {// will be passed to the page component as props
-      pid: context.params.pid,
-      uid: context.params.uid
-    }, 
-  }
+        ))
+      }
+    </Grid>)
 }
